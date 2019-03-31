261a85f9b297faf110467bccc26ac193c7d2159f
fdomateu <49005334+fdomateu@users.noreply.github.com>
2019-03-31 13:51:12 +0200

    Add files via upload

---
+++ LibraryPy/LibraryPyRead.py
@@ -0,0 +1,83 @@
+# FreeCAD LibraryPyRead script of LibraryPy module
+
+#***************************************************************************
+#*   Copyright (c) YEAR YOUR NAME         <Your e-mail address>            *
+#*                                                                         *
+#*   This file is part of the FreeCAD CAx development system.              *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   FreeCAD is distributed in the hope that it will be useful,            *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Lesser General Public License for more details.                   *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with FreeCAD; if not, write to the Free Software        *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************/
+
+class StandardPartInfo():
+
+    "The standard part info from standard part object"
+
+    def __init__(self):
+        self.libraryPyObject = "LibraryPy::StandardPartInfo"
+    
+    def setObjectName(self):
+        return self.libraryObject
+    
+    def setDescription(self, text):
+        self.description = text
+    
+    def getDescription(self):
+        if self.description != None:
+            return self.description
+    
+    def setNorm(self, text):
+        self.norm = text
+    
+    def getNorm(self):
+        if self.norm != None:
+            return self.norm
+
+    def setOldNorm(self, text):
+        self.oldNorm = text
+    
+    def getOldNorm(self):
+        if self.oldNorm != None:
+            return self.oldNorm
+    
+    def setIcon(self, iconFile):
+        self.icon = iconFile
+    
+    def getIcon(self):
+        if self.icon != None:
+            return self.icon
+    
+    def setImage(self, drawingFile):
+        self.image = drawingFile
+    
+    def getImage(self):
+        if self.image != None:
+            return self.image
+    
+    def setGeometry(self, shape):
+        self.geometry = shape
+    
+    def getGeometry(self):
+        if self.geometry != None:
+            return self.geometry
+    
+    def setRefineGeometry(self, shape):
+        self.refineGeometry = shape
+    
+    def getRefineGeometry(self):
+        if self.refineGeometry != None:
+            return self.refineGeometry
